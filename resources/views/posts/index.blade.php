6289d2a58a258c88b0fc4c7999e59a4f46dc8335
Antonio <ameloni9@gmail.com>
2020-05-19 20:11:07 +0200

    Rotta e view con posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+<h2>ciao</h2>
+
+@foreach ($posts as $post)
+  <h2>{{$post->title}}</h2>
+  <img src="{{$post->img}}" alt="">
+  <p>{{$post->body}}</p>
+  <p>{{$post->author}}</p>
+  <small>{{$post->created_at}}</small>
+@endforeach
